958395542d3f1d41ea4effe6281935fbce5c1982
Aaron Addleman <aaronaddleman@gmail.com>
2015-10-21 13:35:26 -0700

    adding rvm detection to prompt for theme aamin

--- themes/aamin/aamin.theme.bash
+++ themes/aamin/aamin.theme.bash
@@ -5,8 +5,14 @@ SCM_THEME_PROMPT_SUFFIX="${cyan})"
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 
+is_rvm_loaded() {
+  if hash rvm 2>/dev/null; then
+    echo "[$(~/.rvm/bin/rvm-prompt)] "
+	fi
+}
+
 prompt() {
-  PS1="\t $(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
+  PS1="\t $(is_rvm_loaded)$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
 }
 
 PROMPT_COMMAND=prompt
