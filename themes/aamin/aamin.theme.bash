ad0d8f2c9acd71d881285486f63adf4e9798200a
Aaron Addleman <aaronaddleman@gmail.com>
2015-10-22 08:42:07 -0700

    fixing theme conflicts

--- themes/aamin/aamin.theme.bash
+++ themes/aamin/aamin.theme.bash
@@ -5,10 +5,6 @@ SCM_THEME_PROMPT_SUFFIX="${cyan})"
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 
-<<<<<<< HEAD
-prompt() {
-  PS1="\t $(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
-=======
 is_rvm_loaded() {
   if hash rvm 2>/dev/null; then
     echo "${cyan}(${red}$(~/.rvm/bin/rvm-prompt)${cyan})${reset_color} "
@@ -17,7 +13,6 @@ is_rvm_loaded() {
 
 prompt() {
   PS1="\t $(is_rvm_loaded)$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
->>>>>>> a5c85cff2edc637188e7078cbcc5f4cec3b91ead
 }
 
 PROMPT_COMMAND=prompt
